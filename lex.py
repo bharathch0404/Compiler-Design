9387d74b829ab1957b93bed0a5b9ab60015e993c
bhavik <bhavikshah406@gmail.com>
2017-02-18 18:32:33 +0530

    mutispace done bhavik

--- lex.py
+++ lex.py
@@ -5,16 +5,19 @@ from sys import argv
 def removecomments():
 	with open ("input.c", "r") as myfile:
 		string=myfile.read()
+
 	string = re.sub(re.compile("[^\"]/\*.*?\*/",re.DOTALL ) ,"" ,string) # remove all occurance streamed comments (/*COMMENT */) from string
 	string = re.sub(re.compile("//.*?\n" ) ,"" ,string) # remove all occurance singleline comments (//COMMENT\n ) from string
-	print string
+	string = re.sub(re.compile("\n{2,}|[\t]+\n|[\t]+") ,"" ,string) # remove multiple spaces
+	
 	string=string.split("\n")
-	string="\n".join(string)
-	print string
+
 	with open("output","w+") as f:
 		for i in string:
-			f.write(i)
+			i+="\n"
+			f.write(i.lstrip())
 	
 
 removecomments()
 
+#Hi!
