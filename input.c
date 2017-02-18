f0caec224253c8bfaba548bd4df08bf21d113a2e
bravikumarreddy <b.ravi.kumar.96@gmail.com>
2017-02-18 19:14:45 +0530

    all

---
+++ input.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+ int main()
+ {
+     //this is a normal c program
+ 	/* "i am removing comments /* and // "  lknfljdjvnjdfnlnl*/ 
+
+
+
+ 	//dfdsvfdv
+ 	/*vdfv
+ 	fddf
+ 	*/
+
+
+
+
+
+
+
+
+
+
+
+
+		printf("/*fdvfdvdf*/")
+    											int a;
+    a = 10;
+    printf("The value of a is %d ",a);
+    return 0;
+ }
