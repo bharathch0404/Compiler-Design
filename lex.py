a6d70add1b3a1d83e0ceccc5addb4a65f2506d91
bhavik <bhavikshah406@gmail.com>
2017-02-18 16:03:49 +0530

    hi this is bhavik

--- lex.py
+++ lex.py
@@ -1,2 +1,20 @@
 import re
+from sys import argv
 #Removing comments
+
+def removecomments():
+	with open ("input.c", "r") as myfile:
+		string=myfile.read()
+	string = re.sub(re.compile("[^\"]/\*.*?\*/",re.DOTALL ) ,"" ,string) # remove all occurance streamed comments (/*COMMENT */) from string
+	string = re.sub(re.compile("//.*?\n" ) ,"" ,string) # remove all occurance singleline comments (//COMMENT\n ) from string
+	print string
+	string=string.split("\n")
+	string="\n".join(string)
+	print string
+	with open("output","w+") as f:
+		for i in string:
+			f.write(i)
+	
+
+removecomments()
+
